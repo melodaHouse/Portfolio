1bd05b419ad449950f4a664838ef5360d9cf0eef
Akash Chowdhury <emailidakashchowdhury@gmail.com>
2025-12-30 22:32:49 +0530

    upload

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 
-const baseUrl = process.env.apiURL || 'http://backend.melodahouse.com/api/v1';
+const baseUrl = process.env.apiURL || 'https://backend.melodahouse.com/api/v1';
 const apiUri= `${baseUrl}/portfolio`;
 
 export default apiUri;
